eecd7e87f78f90b62897b8ae1ccbb3d6a6abe0f2
Samra Junejo <samrajunejo1@gmail.com>
2023-02-15 19:08:28 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+//Activity 1
+ //const logMsg = () => {
+    // console.log("Hello CodeNation")
+ //}
+//logMsg()
+
+ //function Multi(happy) {
+  //  for (i = 0; i < 4; i++) {
+  //      happy()
+  //  }
+//}
+ //Multi(logMsg)
+
+//Activity 2
+// let numbers = [5, 8, 6, 19, 56];
+
+// let newNums = numbers.map( (num) => {
+//        return num * 3;
+// } )
+
+// console.log(newNums)
+
+//Activity 3
+ //const add = (a, b) => {
+  //  return a + b;
+  // };
+//const subtract = (a, b) => {
+  //  return a - b;
+  //  };
+//const multiply = (a, b) => {
+   //  return a * b;
+  // };
+//const divide = (a, b) => {
+   // return a / b;
+  // };
+//const doMaths = (num1) => { 
+  // return (num2, no) => { 
+  //  return no(num1, num2);
+  // };
+ // }
+
+  //console.log( doMaths(2) (5, multiply) ) 
